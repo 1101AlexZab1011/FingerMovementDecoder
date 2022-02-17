023063e94061f4551bedd6beb3f42d7f22e06de9
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-02-17 13:31:34 +0300

    interactive tool for sp weights visualization

--- lfcnn_vis.py
+++ lfcnn_vis.py
@@ -27,7 +27,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import copy
 import scipy.signal as sl
-# from mat_to_fif import read_pkl
+from matplotlib.widgets import Button
 
 def read_pkl(path: str) -> Any:
     with open(
@@ -312,6 +312,162 @@ def plot_tempospectral(
         fig.suptitle(title, fontsize=20)
     
     return fig
+
+
+def plot_spatial_weights(
+    spatial_parameters: SpatialParameters,
+    temporal_parameters: TemporalParameters,
+    info: mne.Info,
+    summarize: Optional[str] = 'sum',
+    title: Optional[str] = 'Spatial Patterns',
+    show: Optional[bool] = True,
+    logscale: Optional[bool] = False
+) -> Union[mp.figure.Figure, NoReturn]:
+    
+    mp.use('Qt5Agg')
+    
+    def init_canvas(ax01, ax02, ax1, ax2):
+        ax01.axes.xaxis.set_visible(False)
+        ax01.axes.yaxis.set_visible(False)
+        ax02.axes.xaxis.set_visible(False)
+        ax02.axes.yaxis.set_visible(False)
+
+        ax01.spines['right'].set_visible(False)
+        ax01.spines['left'].set_visible(False)
+        ax01.spines['top'].set_visible(False)
+        ax01.spines['bottom'].set_visible(False)
+        ax02.spines['right'].set_visible(False)
+        ax02.spines['left'].set_visible(False)
+        ax02.spines['top'].set_visible(False)
+        ax02.spines['bottom'].set_visible(False)
+
+        ax1.spines['right'].set_visible(False)
+        ax1.set_xlabel('Channels')
+        ax1.set_ylabel('Latent Sources')
+        ax1.set_yticks(np.arange(y_lim))
+        ax1.set_yticklabels(labels = [i+1 for i in sorting_callback.sorted_indices])
+
+        ax2.axes.yaxis.set_visible(False)
+        ax2.axes.xaxis.set_visible(False)
+        ax2.spines['left'].set_visible(False)
+        
+    class SortingCallback:
+            
+        def __init__(self, button: Button, fig: plt.Figure, bar_ax: mp.axes.Axes, imshow_ax: mp.axes.Axes, indices: list[int]):
+            self._button = button
+            self._fig = fig
+            self._bar_ax = bar_ax
+            self._imshow_ax = imshow_ax
+            self._event = None
+            self._sorted_indices = indices
+        
+        def __call__(self, event):
+            self._event = event
+
+            if '▼' in self._button.label._text:
+                self.decreasing()
+            else:
+                self.increasing()
+        
+        @property
+        def sorted_indices(self):
+            return self._sorted_indices
+        @sorted_indices.setter
+        def sorted_indices(self, value):
+            raise AttributeError('Impossible to set indices directly')
+
+        def increasing(self):
+            self._button.label.set_text('Sort ▼')
+            self._sorted_indices = sorted(range(len(sums)), key=lambda k: sums[k], reverse=True)
+            self.update()
+
+        def decreasing(self):
+            self._button.label.set_text('Sort ▲')
+            self._sorted_indices = sorted(range(len(sums)), key=lambda k: sums[k])
+            self.update()
+        
+        def update(self):
+            self._imshow_ax.clear()
+            self._imshow_ax.imshow(data.T[self._sorted_indices, :], aspect='auto', cmap='RdBu_r')
+            self._bar_ax.clear()
+            self._bar_ax.barh(range(len(sums)), np.abs(sums)[self._sorted_indices], color=colors[self._sorted_indices], height=.9)
+            init_canvas(ax01, ax02, ax1, ax2)
+            self._fig.canvas.draw()
+    
+    def onclick(event):
+        
+        if ax1.lines:
+            
+            for i in range(len(ax1.lines)):
+                ax1.lines.remove(ax1.lines[i])
+                
+        _, iy = event.xdata, event.ydata
+        
+        if event.xdata is not None and event.ydata is not None and 0 < event.xdata < x_lim and 0 < event.ydata < y_lim:
+            iy = int(np.rint(iy))
+            color = colors[sorting_callback._sorted_indices[iy]]
+            line = mp.lines.Line2D([0, x_lim], [iy, iy], color=color, linewidth=16, alpha=.4)
+            ax1.add_line(line)
+            fig1.canvas.draw()
+            fig2, (ax21, ax22) = plt.subplots(ncols=2, nrows=1)
+            plot_patterns(data, info, sorting_callback.sorted_indices[iy], ax21, name_format='', title='')
+            ax22.plot(
+                                temporal_parameters.franges,
+                                temporal_parameters.finputs[sorting_callback.sorted_indices[iy]],
+                                temporal_parameters.franges,
+                                temporal_parameters.foutputs[sorting_callback.sorted_indices[iy]],
+                                temporal_parameters.franges,
+                                temporal_parameters.fresponces[sorting_callback.sorted_indices[iy]],
+                            )
+            ax22.legend(['Filter input', 'Filter output', 'Filter responce'], loc='upper right')      
+            ax22.spines['top'].set_visible(False)
+            ax22.spines['right'].set_visible(False)
+            
+            if logscale:
+                ax22.set_aspect(25)  
+                ax22.set_yscale('log')
+            else:
+                ax22.set_aspect(75)  
+                
+            fig2.suptitle(f'Latent source {sorting_callback.sorted_indices[iy] + 1}')
+            plt.show()
+
+    data = spatial_parameters.patterns.copy()
+    x_lim, y_lim = data.shape
+    fig1 = plt.figure()
+    fig1.suptitle(title)
+    gs = fig1.add_gridspec(2, 2, hspace=0, wspace=0, width_ratios=[1, .1], height_ratios=[.025, 1])
+
+    (ax01, ax02), (ax1, ax2) = gs.subplots(sharex='col', sharey='row')
+
+    if summarize == 'sum':
+        sums = np.sum(data, axis=0)
+    elif summarize == 'sumabs':
+        sums = np.sum(np.abs(data), axis=0)
+    elif summarize == 'abssum':
+        sums = np.abs(np.sum(data, axis=0))
+    else:
+        raise NotImplementedError(f'The "{summarize}" method not implemented. Available methods: "sum", "sumabs", "abssum"')
+    colors = np.array(['#f2827a' if sum_ >= 0 else '#8bbae5' for sum_ in sums])
+
+    sort_button = Button(ax02, 'Sort')
+    sorting_callback = SortingCallback(sort_button, fig1, ax2, ax1, sorted(range(len(sums)), reverse=True))
+    sort_button.on_clicked(sorting_callback)
+
+    init_canvas(ax01, ax02, ax1, ax2)
+
+    ax1.imshow(data.T, aspect='auto', cmap='RdBu_r')
+    ax2.barh(sorting_callback.sorted_indices, np.abs(sums), color=colors, height=.9)
+
+    cid1 = fig1.canvas.mpl_connect('button_press_event', onclick)
+    cid2 = fig1.canvas.mpl_connect('close_event', lambda e: fig1.canvas.mpl_disconnect(cid1))
+    
+    if show:
+        plt.show()
+    
+    fig1.canvas.mpl_disconnect(cid2)
+    
+    return fig1
 
 
 if __name__ == '__main__':
