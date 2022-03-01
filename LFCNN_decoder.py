395c6f652bf67226494ea6051dc8c6675b6097a9
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-03-01 11:54:28 +0300

    nt

--- LFCNN_decoder.py
+++ LFCNN_decoder.py
@@ -341,6 +341,7 @@ if __name__ == '__main__':
         train_loss_, train_acc_ = model.evaluate(meta['train_paths'])
         test_loss_, test_acc_ = model.evaluate(meta['test_paths'])
         model.compute_patterns(meta['train_paths'])
+        nt = model.dataset.h_params['n_t']
         time_courses = np.squeeze(model.lat_tcs.reshape([model.specs['n_latent'], -1, nt]).mean(1))
         times = (1/float(model.dataset.h_params['fs']))*np.arange(model.dataset.h_params['n_t'])
         patterns = model.patterns.copy()
