0b5341c07c03a7b21af1142ea4560b32b68ad3dc
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-03-10 13:58:34 +0300

    slfcnn

--- LFRNN_decoder.py
+++ LFRNN_decoder.py
@@ -171,6 +171,7 @@ class LFRNN(BaseModel):
         #     Dense(size=self.out_dim, nonlin=tf.identity, specs=self.specs)
         # )
         # sLFCNN
+        print(self.inputs.shape, self.inputs.shape[3])
         self.design = ModelDesign(
             self.inputs,
             DeMixing(size=self.specs['n_latent'], nonlin=tf.identity, axis=3, specs=self.specs),
@@ -182,7 +183,7 @@ class LFRNN(BaseModel):
                     padding=self.specs['padding'],
                     specs=self.specs
                 ),
-            tf.keras.layers.DepthwiseConv2D((1, self.inputs.shape[3]), padding='valid', activation='relu', kernel_regularizer='l1'),
+            tf.keras.layers.DepthwiseConv2D((1, 200), padding='valid', activation='relu', kernel_regularizer='l1'),
             tf.keras.layers.Dropout(self.specs['dropout'], noise_shape=None),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(self.out_dim, kernel_regularizer='l1'),
