171f99ca2cb72107ca434ca997ef3e4fca55cc27
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-05 16:26:20 +0300

    bash script to run lfcnn

--- lfcnn_run.sh
+++ lfcnn_run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash 
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8
 
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8_7Hz -hp 7
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix B1-B8_7Hz -hp 7
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8_7Hz -hp 7
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix B1-B8_7Hz -hp 7
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8_7Hz -hp 7
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix B1-B8_7Hz -hp 7
 
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8_7Hz -hp 7
-./FingerMovementDecoder/venv/bin/python ./FingerMovementDecoder/LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8_7Hz -hp 7
+./venv/bin/python ./LFCNN_concatenated.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix B1-B8_7Hz -hp 7
