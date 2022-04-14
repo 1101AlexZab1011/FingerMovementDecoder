dabb0940d1356fcce102607e232f30d650a54109
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-14 21:25:14 +0300

    lfcnn sequence

---
+++ lfcnn-sequence.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B7 B8 B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B5 B6 B7 B8 B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 -cmb "RI" "RM" "LI" "LM" --postfix seq
+
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B7 B8 B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B5 B6 B7 B8 B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 -cmb "RI RM" "LI LM" --postfix seq
+
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "LI" "LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B7 B8 B9 B10 B11 B12 -cmb "LI" "LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B5 B6 B7 B8 B9 B10 B11 B12 -cmb "LI" "LM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 -cmb "LI" "LM" --postfix seq
+
+./venv/bin/python ./LFCNN_decoder.py -es B9 B10 B11 B12 -cmb "RI" "RM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B7 B8 B9 B10 B11 B12 -cmb "RI" "RM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B5 B6 B7 B8 B9 B10 B11 B12 -cmb "RI" "RM" --postfix seq
+./venv/bin/python ./LFCNN_decoder.py -es B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 -cmb "RI" "RM" --postfix seq
