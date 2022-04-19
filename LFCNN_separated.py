f6ed37bb2590fe3cfab88fce162eb1e08c001d38
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-19 15:18:38 +0300

    lfcnn_separated small fixes

--- LFCNN_separated.py
+++ LFCNN_separated.py
@@ -333,10 +333,10 @@ if __name__ == '__main__':
                 perf_tables_path,
                 f'{classification_name_formatted}_sep.xlsx'
             )
-            with pd.ExcelWriter(perf_table_path) as writer: 
+            with pd.ExcelWriter(perf_table_path, engine='openpyxl') as writer: 
                 if os.path.exists(perf_table_path):
-                    pd.concat([pd.read_excel(perf_table_path, index_col=0, header=0, sheet_name=sheet_name, engine='openpyxl'), processed_df], axis=0)\
-                    .to_excel(writer, sheet_name=sheet_name, engine='openpyxl')
+                    pd.concat([pd.read_excel(perf_table_path, index_col=0, header=0, sheet_name=sheet_name), processed_df], axis=0)\
+                    .to_excel(writer, sheet_name=sheet_name)
                 else:
                     processed_df\
                     .to_excel(writer, sheet_name=sheet_name, engine='openpyxl')
