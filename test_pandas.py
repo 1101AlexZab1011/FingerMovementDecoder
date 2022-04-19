00ac2ab302baefe7568c0c9854360284f9fb3e2e
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-19 15:26:53 +0300

    lfcnn_separated small fixes

---
+++ test_pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import os
+
+data1 = np.random.rand(10, 10)
+data2 = np.random.rand(10, 10)
+data3 = np.random.rand(10, 10)
+
+
+root = '../'
+with pd.ExcelWriter(os.path.join(root, 'testfile.xlsx')) as writer:
+    for data, sheet in zip([data1, data2, data3], ['sheet1', 'sheet2', 'sheet3']):
+        df = pd.DataFrame(data, columns=[f'col {i}' for i in range(data1.shape[0])])
+        df.to_excel(writer, sheet_name=sheet, engine='openpyxl')
