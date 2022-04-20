5f382654ff22ec0e8e2f8e58b5d15df277ec696a
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-20 23:07:29 +0300

    memorytask reader

--- memorytask/validate_epochs.py
+++ memorytask/validate_epochs.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     for subject in os.listdir(subjects_dir):
         epochs_dir = os.path.join(subjects_dir, subject, 'Epochs')
         for epochs_file in os.listdir(epochs_dir):
-            print(f'{epochs_file}: {mne.read_epochs(epochs_file).get_data().shape}')
+            print(f'{epochs_file}: {mne.read_epochs(os.path.join(epochs_dir, epochs_file)).get_data().shape}')
