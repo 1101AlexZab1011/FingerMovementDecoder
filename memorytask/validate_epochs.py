6e5bc6b8bebabdc4bacc61bac990278197d64daf
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-20 23:12:04 +0300

    memorytask reader

--- memorytask/validate_epochs.py
+++ memorytask/validate_epochs.py
@@ -13,7 +13,11 @@ if __name__ == '__main__':
     subjects_dir = './Source/MemoryTaskSubjects'
     for subject in os.listdir(subjects_dir):
         epochs_dir = os.path.join(subjects_dir, subject, 'Epochs')
+        out = dict()
         for epochs_file in os.listdir(epochs_dir):
             with Silence():
                 shape = mne.read_epochs(os.path.join(epochs_dir, epochs_file)).get_data().shape
-            print(f'{epochs_file}: {shape}')
+            out[epochs_file[:9]] = shape[0]
+        for sti, samp in out.items():
+            print(f'{sti}: {samp}', end=';')
+        print()
