eb89653001bf0e9e9eb012cb4de68e4c7362a799
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-20 23:08:13 +0300

    memorytask reader

--- memorytask/validate_epochs.py
+++ memorytask/validate_epochs.py
@@ -7,10 +7,12 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 
 import mne
+from utils.console import Silence
 
 if __name__ == '__main__':
     subjects_dir = './Source/MemoryTaskSubjects'
     for subject in os.listdir(subjects_dir):
         epochs_dir = os.path.join(subjects_dir, subject, 'Epochs')
         for epochs_file in os.listdir(epochs_dir):
-            print(f'{epochs_file}: {mne.read_epochs(os.path.join(epochs_dir, epochs_file)).get_data().shape}')
+            with Silence():
+                print(f'{epochs_file}: {mne.read_epochs(os.path.join(epochs_dir, epochs_file)).get_data().shape}')
