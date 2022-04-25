2160e1f32868b8254871547d4ff748e01f74cbfa
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2022-04-25 13:23:05 +0300

    memorytask reader

---
+++ iter_decode_sep.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# LFCNN
+
+# 4 cases
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "LI" "LM" "RI" "RM" --no-params
+    done
+
+# left vs right
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "LI LM" "RI RM" --no-params
+    done
+
+# left
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "LI" "LM" --no-params
+    done
+
+# right
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "RI" "RM" --no-params
+    done
+
+
+# LFRNN
+
+# 4 cases
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "LI" "LM" "RI" "RM" --no-params -m LFRNN --postfix LFRNN
+    done
+
+# left vs right
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "LI LM" "RI RM" --no-params -m LFRNN --postfix LFRNN
+    done
+
+# left
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "LI" "LM" --no-params -m LFRNN --postfix LFRNN
+    done
+
+# right
+for i in {0..10}
+    do 
+        ./venv/bin/python ./LFCNN_separated.py -cms "B1 B2" "B7 B8" -cmc "RI" "RM" --no-params -m LFRNN --postfix LFRNN
+    done
+
+
+# seq
+for i in {0..10}
+    do 
+        ./lfcnn-sequence.sh
+    done
+
+for i in {0..10}
+    do 
+        ./lfrnn-sequence.sh
+    done
